722272ba497c40cbc47bf31b478f3ac9746c8649
Mitch R <olordhux+github@gmail.com>
2014-08-14 00:00:43 -0700

    basic module loading.
    forget promises for fs application-- not really needed.

--- src/core.js
+++ src/core.js
@@ -2,8 +2,10 @@
 
 var Prom = require('bluebird'); // "Prom" avoids conflict with es6 promises
 var fs = require('fs');
-var readFile = Prom.promisify(fs.readFile);
-var readDir = Prom.promisify(fs.readdir);
+var cp = require('child_process');
+
+// var readFile = Prom.promisify(fs.readFile);
+// var readDir = Prom.promisify(fs.readdir);
 
 var Router = require('./router');
 var Client = require('./client');
@@ -12,9 +14,13 @@ var Client = require('./client');
 var r;
 var irc;
 
+var loadedModules = {};
+
 var init = function() {
   // Read config and instantiate irc client and router
-  readFile(__dirname + "/../config.json", "utf8").then(function(data) {
+  fs.readFile(__dirname + "/../config.json", "utf8", function(err, data) {
+    if(err) console.log('error loading config file:', err);
+
     var config = JSON.parse(data);
     r = new Router();
 
@@ -27,16 +33,30 @@ var init = function() {
     loadPlugins(config.pluginDir);
     // TODO: fix this error handling-- split it up? we get to this catch if any
     // module failes loading.
-  }).catch(function(err) {
-    console.error('Init error:', err);
   });
 };
 
 var loadPlugins = function(pluginDir) {
-  readDir(__dirname + '/' + pluginDir).then(function(plugins) {
-    console.log(plugins);
-  }).catch(function(err) {
-    console.log(err);
+  var fullPluginDir = __dirname + '/' + pluginDir;
+
+  fs.readdir(fullPluginDir, function(err, plugins) {
+    if(err) console.log('error reading plugin dir:', err);
+
+    plugins.forEach(function(pluginFile) {
+      var pluginProcess = cp.fork(fullPluginDir + pluginFile);
+
+      loadedModules[pluginFile] = {
+        process: pluginProcess,
+        timeLoaded: new Date(),
+        pid: pluginProcess.pid,
+        state: 'running',
+        routes: {}
+      };
+
+      pluginProcess.on('message', function(message) {
+        // pass message on to router
+      });
+    });
   });
 };
 
