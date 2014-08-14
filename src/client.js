3d90f90745b5b8f0cf75339b87f46a354512f4c4
Mitch R <olordhux+github@gmail.com>
2014-08-14 15:57:24 -0700

    basic client config and listeners

--- src/client.js
+++ src/client.js
@@ -2,9 +2,26 @@
 var irc = require('irc');
 
 var ircClient = function(config) {
-  this.client = new irc.Client(config.network, config.name, {
-    channels: config.channels
-  });  
+  var self = this;
+  self.name = config.name;
+  self.network = config.network;
+  self.channels = config.channels;
+
+  self.client = new irc.Client(self.network, self.name, {
+    channels: self.channels
+  });
+
+  self.client.addListener('error', function(message) {
+    console.error('IRC Error:', message);
+  });
+
+  self.client.addListener('message', function(from, to, message) {
+    console.log('%s => %s: %s', from, to, message);
+    if(to === self.name) {
+        console.log('Got private message from %s: %s', from, message);
+    }
+    
+  });
 };
 
 module.exports = ircClient;
