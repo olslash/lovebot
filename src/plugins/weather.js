29476dddf6f0b02d9caea0730fc40d82621744ab
Mitch R <olordhux+github@gmail.com>
2014-08-14 18:40:01 -0700

    testing functionality

--- src/plugins/weather.js
+++ src/plugins/weather.js
@@ -1,1 +1,15 @@
-process.send('hello from the plugin')
+'use strict';
+
+// process.send({message: 'hello from the weather plugin'});
+
+process.send({
+  register: { 
+    moduleName: 'weatherman',
+    commands: ['weather', 'we', 'wea'],
+    accessLevel: 3 // for future auth layer
+  }
+});
+
+process.on('message', function(message) {
+  console.log('weather plugin recieved message', message);
+});
