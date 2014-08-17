1436fc5a4e9451fca8baa8cf5dcecab3860c9cf4
Mitch R <olordhux+github@gmail.com>
2014-08-16 19:15:45 -0700

    mock of basic plugin function

--- src/plugins/weather.js
+++ src/plugins/weather.js
@@ -1,15 +1,21 @@
 'use strict';
 
-// process.send({message: 'hello from the weather plugin'});
+process.send({message: 'hello from the weather plugin'});
 
 process.send({
   register: { 
-    moduleName: 'weatherman',
+    // moduleName: 'weatherman',
     commands: ['weather', 'we', 'wea'],
     accessLevel: 3 // for future auth layer
   }
 });
 
+process.send({message: 'hi'});
+
 process.on('message', function(message) {
   console.log('weather plugin recieved message', message);
+  process.send({
+    messageId: message.messageId,
+    replyText: 'this is a reply from weather'
+  });
 });
