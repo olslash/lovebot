1f7c918a51d5e896ac145587377fbb09669ae1c3
Mitch R <olordhux+github@gmail.com>
2014-08-27 21:25:08 -0700

    test that router can differentiate between valid and invalid plugin messages

--- test/test-router.js
+++ test/test-router.js
@@ -14,11 +14,12 @@ describe('Router', function() {
     router = new botRouter();
   });
 
-  it('should have register, unregister, routeincoming and routeoutgoing methods', function() {
-    expect(router.register).to.be.a('function');
-    expect(router.unregister).to.be.a('function');
-    expect(router.routeIncoming).to.be.a('function');
-    expect(router.routeOutgoing).to.be.a('function');
+  it('should have register, unregister, routeincoming and routeoutgoing methods', 
+    function() {
+      expect(router.register).to.be.a('function');
+      expect(router.unregister).to.be.a('function');
+      expect(router.routeIncoming).to.be.a('function');
+      expect(router.routeOutgoing).to.be.a('function');
   });
 
   it('should confirm registration of new commands', function() {
@@ -30,7 +31,7 @@ describe('Router', function() {
     expect(router.register({}, ['test', 'wea'])).to.be.false;
   });
 
-  it('should route incoming messages to registered commands', function(done) {
+  it('should route incoming messages to the correct plugin', function(done) {
     var fakePlugin = {
       process: {
         send: function(routingObject) {
@@ -52,5 +53,44 @@ describe('Router', function() {
 
       done();
     }
+  });
+
+  it('should notify the core of valid plugin responses with a message event', 
+    function(done) {
+      var fakePlugin = {
+        process: {
+          send: function(routingObject) {
+            forwardReceivedReply(routingObject);
+          },
+          filename: 'fakeplugin'
+        }
+      };
+
+      var invalidReply = {
+        messageId: 'badmsgid',
+        replyText: 'this is an invalid plugin reply.'
+      };
+
+      router.on('message', checkRouterReply);
+      
+      router.register(fakePlugin, ['echo']);
+      router.routeOutgoing(invalidReply); // this should do nothing
+      router.routeIncoming('from', 'to', 'echo 123 hi mom');
+
+      function forwardReceivedReply(routingObject) {
+        var messageId = routingObject.messageId;
+        var replyText = routingObject.messageText;
+
+        router.routeOutgoing({
+          messageId: messageId,
+          replyText: replyText
+        });
+      }
+
+      function checkRouterReply(to, message) {
+        expect(to).to.equal('to');
+        expect(message).to.equal('123 hi mom');
+        done();
+      }
   });
 });
